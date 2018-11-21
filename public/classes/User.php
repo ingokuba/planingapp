a0bcbb96a83b4e760b4e8f7ee26c2e9bf4c33d9e
Ingo Kuba <ikuba@intension.de>
2018-11-21 09:39:41 +0100

    2: Login page to store user in session.

--- public/classes/User.php
+++ public/classes/User.php
@@ -3,6 +3,10 @@
 class User
 {
 
+    public $USER = "User";
+
+    public $EMAIL = "email";
+
     private $model;
 
     private $givenname;
@@ -12,6 +16,8 @@ class User
     private $email;
 
     private $password;
+
+    private $createdAt;
 
     public function __construct(PlaningModel $model)
     {
@@ -28,19 +34,19 @@ class User
     public function store(bool $override): string
     {
         $response = "";
-        $result = $this->model->select("User", "*", "email", "'$this->email'");
+        $result = $this->model->select($this->USER, "*", $this->EMAIL, "'$this->email'");
         // check if user already exists:
         if ($result == null) {
             if (! $this->isStorable()) {
                 $response = "User is missing attributes.";
             } else {
                 // store new user:
-                $response = $this->model->insert("User", "givenName, surname, email, password", "'$this->givenname', '$this->surname', '$this->email', '$this->password'");
+                $response = $this->model->insert($this->USER, "givenName, surname, email, password", "'$this->givenname', '$this->surname', '$this->email', '$this->password'");
             }
         } else if ($override) {
             $id = $result["userID"];
             // update existing user:
-            if (! $this->model->update("User", "userID=$id", "givenName='$this->givenname', surname='$this->surname', email='$this->email', password='$this->password'")) {
+            if (! $this->model->update($this->USER, "userID=$id", "givenName='$this->givenname', surname='$this->surname', email='$this->email', password='$this->password'")) {
                 $response = "Updating the user was not successful.";
             }
         } else {
@@ -62,9 +68,68 @@ class User
         return true;
     }
 
+    /**
+     * Login the user to the session.
+     *
+     * @return string Error message.
+     */
+    public function login(): string
+    {
+        $result = $this->model->select($this->USER, "*", "$this->EMAIL", "'$this->email'");
+        if ($result != null && $result["password"] == $this->password) {
+            // Cookie lifespan: 30 minutes
+            setcookie($this->USER, "$this->email", time() + 1800, "/");
+            header("Location: /");
+            return "";
+        }
+        return "Invalid credentials.";
+    }
+
+    /**
+     * Logout the user in the session.
+     */
+    public function logout()
+    {
+        $cookie = $_COOKIE[$this->USER];
+        if (isset($cookie)) {
+            setcookie($this->USER, '', time() - 1000);
+        }
+    }
+
+    public function getUserFromSession()
+    {
+        $this->email = $this->getCookieValue($this->USER); // get from cookie
+        if (empty($this->email)) {
+            return null;
+        }
+        $result = $this->model->select($this->USER, "*", $this->EMAIL, "'$this->email'");
+        if ($result != null) {
+            $this->givenname = $result["givenName"];
+            $this->surname = $result["surname"];
+            $this->createdAt = $result["createdAt"];
+            return $this;
+        }
+        // User not found.
+        return null;
+    }
+
+    private function getCookieValue(string $name): string
+    {
+        $cookie = $_COOKIE[$name];
+        if (isset($cookie)) {
+            return $cookie;
+        }
+        return "";
+    }
+
     public function setGivenname(string $value)
     {
         $this->givenname = $value;
+    }
+
+    public function getGivenname(): string
+    {
+        return $this->givenname;
     }
 
     public function setSurname(string $value)
@@ -72,9 +137,24 @@ class User
         $this->surname = $value;
     }
 
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
     public function setEmail(string $value)
     {
         $this->email = $value;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
     }
 
     public function setPassword(string $value)
--- public/views/Page.php
+++ public/views/Page.php
@@ -13,6 +13,8 @@ class Page implements IPage
 
     protected $controller;
 
+    protected $user;
+
     public $body;
 
     public function __construct(PlaningController $controller, PlaningModel $model)
@@ -23,13 +25,44 @@ class Page implements IPage
 
     private function getBody(): string
     {
+        // initialize user:
+        $this->getUser();
+        $loginBlock = "<div class='row'><button class='btn btn-lg mb-1' data-toggle='tooltip' title='Login' onclick='window.location=\"Login\";'><i class='fas fa-user-lock'></i></button>
+                        <i class='fas fa-arrow-left ml-2 pt-3'> Login here</i></div>";
+        if ($this->user != null) {
+            $givenname = $this->user->getGivenname();
+            $surname = $this->user->getSurname();
+            $createdAt = date("d.m.Y", strtotime($this->user->getCreatedAt()));
+            $loginBlock = "<div class='row'><button class='btn btn-lg mb-1' data-toggle='tooltip' title='Logout' onclick='document.cookie=\"User=; expires=new Date(); path=/;\";location.reload();'><i class='fas fa-sign-out-alt'></i></button>
+                        <div class='ml-2 pt-3'>Welcome <b>$givenname $surname</b> (Member since $createdAt)</div>";
+        }
+        // no body defined -> default
         if ($this->body == null) {
             // image source: https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/345eyrhfj.png?itok=35gvnyvU&resize=1100x1100
-            $this->body = "<button class='btn btn-lg mb-1' data-toggle='tooltip' title='Login' onclick='window.location=\"Registration\";'><i class='fas fa-user-lock'></i></button>
+            $this->body = "$loginBlock
                             <img src='templates/pokerdogs.png' class='img-fluid' alt='image not found'>
                             <h1 class='mt-4 text-center'>Cost estimation game for software projects.</h1>";
         }
         return $this->body;
+    }
+
+    private function getUser()
+    {
+        $this->user = new User($this->model);
+        $this->user = $this->user->getUserFromSession();
+    }
+
+    /**
+     * Should be called by child classes that require a logged out context.
+     *
+     * @see Registration
+     * @see Login
+     */
+    protected function logout()
+    {
+        if ($this->user != null) {
+            $this->user->logout();
+        }
     }
 
     public function output(): string
